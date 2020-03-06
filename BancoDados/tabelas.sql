99747d2d721b1cc043d59e10b0ff5bba9646258e
NICOLY <nicolyfalmeida12@gmail.com>
2020-03-06 08:32:31 -0300

    Crando as tabelas e inserindo dados

---
+++ BancoDados/tabelas.sql
@@ -0,0 +1,4 @@
+coamndos SQL tabela 01
+comandos SQL tabelas 02
+coamndos SQL tabela 03
+comandos de inclusão nde dados
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 alguma coisa
 
 
-nova qualquer coisa
+nova qualquer connection_status
+
+comandos php_check_syntax
+
+
+novos comandos
+
+temos varios comandos novos e legais
